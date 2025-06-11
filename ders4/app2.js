1a9e76abfa3084732a4356766f5f64272291b3af
Muhammed Samed Çağlı <136382731+msamedcagli@users.noreply.github.com>
2025-06-11 09:39:41 +0300

    Update app2.js

--- ders4/app2.js
+++ ders4/app2.js
@@ -1,1 +1,26 @@
 const url = require('url');
+
+const address = 'https://github.com/msamedcagli/nodeJS';
+
+let result = url.parse(address,true);
+console.log(result);
+
+/* 
+çıktı:
+
+Url {
+  protocol: 'https:',
+  slashes: true,
+  auth: null,
+  host: 'github.com',
+  port: null,
+  hostname: 'github.com',
+  hash: null,
+  search: null,
+  query: [Object: null prototype] {},
+  pathname: '/msamedcagli/nodeJS',
+  path: '/msamedcagli/nodeJS',
+  href: 'https://github.com/msamedcagli/nodeJS'
+}
+
+*\
